1dc24ae58b7d852cf41583e18b0f8dfa9b782d6c
Klay <arqkdias@gmail.com>
2023-04-13 17:31:57 -0400

    -

--- Teste_survey.py
+++ Teste_survey.py
@@ -1,10 +1,22 @@
-import googlemaps 
+import xml.etree.ElementTree as ET 
+# Carregando o arquivo XML 
+tree = ET.parse('arquivo.xml') 
+root = tree.getroot() 
+result = 3
 
-gmaps = googlemaps.Client(key='AIzaSyA2cS5CMyuGcE6PFmMTvUWf8S8AwcVdFBM')
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((-27.75722428, -48.51074237))
+elementos_filhos = root.findall('caixasEntrada')
+for elemento_filho in elementos_filhos:
+    for child in elemento_filho:
+        for i in child:
+            if i.tag =='prumadas':
+                for filho in i:
+                    for prum in filho:
+                        if prum.tag == 'ucs':
+                            for lista in range(result):
+                                novo_elemento_filho = ET.Element('uc')
+                                novo_elemento_filho.text = '\n'
+                                prum.insert(1,novo_elemento_filho)
+                            
+tree.write('edificio1//arquivo.xml')
 
 
-for i in reverse_geocode_result[1]:
-    print(i)
-        
